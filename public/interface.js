db8bfef8b194fbb0946e1d1255f5a4ba094b077b
Alex <alex@scotttonge.com>
2017-10-27 16:33:44 +0100

    Beautified some files

--- public/interface.js
+++ public/interface.js
@@ -34,14 +34,13 @@ $( document ).ready(function() {
            return 'green';
         }
         else if (thermo._temperature >= 18 && thermo._temperature <= 24) {
-           return 'orange';
+           return 'blue';
         }
         else if (thermo._temperature >= 24) {
            return 'red';
         }
-      });
+    });
   });
-
 
   $('#select-city').submit(function(event) {
     event.preventDefault();
@@ -49,27 +48,13 @@ $( document ).ready(function() {
     displayWeather(city);
   });
 
-
   function displayWeather(city) {
- var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
- var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
- var units = '&units=metric';
- $.get(url + token + units, function(data) {
-   $('#city-temperature').text(data.main.temp);
- });
- }
+     var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+     var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+     var units = '&units=metric';
+     $.get(url + token + units, function(data) {
+     $('#city-temperature').text(data.main.temp);
+    });
+  }
 
 });
-
-
-// $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=560af2d8a5d3aa69f69a24cdcd57f6f1&units=metric', function(data) {
-//   $('#current-temperature').text(data.main.temp);
-// });
-
-//   $('#select-city').submit(function(event) {
-//   event.preventDefault();
-//   var city = $('#current-city').val();
-//   $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
-//     $('#current-temperature').text(data.main.temp);
-//   });
-// });
